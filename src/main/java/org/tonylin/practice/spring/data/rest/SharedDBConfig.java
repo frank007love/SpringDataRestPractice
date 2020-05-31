67d9e8e02ea0104ca592dd23f641ab4ea4bc06cb
TonyLin <TonyLin@TonyLin-Desktop>
2020-05-31 19:01:52 +0800

    extract spring shardDBConfig

---
+++ src/main/java/org/tonylin/practice/spring/data/rest/SharedDBConfig.java
@@ -0,0 +1,67 @@
+package org.tonylin.practice.spring.data.rest;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@EnableTransactionManagement
+@ComponentScan
+@EnableJpaRepositories
+@PropertySource(value={"classpath:postgres.properties","file:./config/postgres.properties"}, ignoreResourceNotFound=true)
+public class SharedDBConfig {
+	private static Logger logger = LoggerFactory.getLogger(SharedDBConfig.class);
+	@Autowired
+	Environment env;
+
+	@Bean
+	public DataSource dataSource() {
+		
+		BasicDataSource ds = new BasicDataSource();
+		// 
+		ds.setDriverClassName(env.getProperty("jdbc.driverClassName"));
+		
+		ds.setUrl(env.getProperty("jdbc.url"));
+		ds.setUsername(env.getProperty("jdbc.username"));
+		ds.setPassword(env.getProperty("jdbc.password"));
+		
+		logger.debug("db url: {}", env.getProperty("jdbc.url"));
+		return ds;
+	}
+	
+	@Bean
+	public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setDatabase(Database.POSTGRESQL);
+		vendorAdapter.setGenerateDdl(true);
+
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setJpaVendorAdapter(vendorAdapter);
+		factory.setPackagesToScan(getClass().getPackage().getName());
+		factory.setDataSource(dataSource());
+
+		return factory;
+	}
+	
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactory().getObject());
+		return txManager;
+	}
+}
